9211cb62d4eda83d705da208872818051ed51c81
JD Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2023-02-13 05:44:05 +1100

    Add example index.ts with valid user

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import matrixClient from './matrixClient';
+
+(async () => {
+  console.log('Running...');
+
+  const baseUrl = 'https://matrix.msnld.com';
+  const username = 'testuser';
+  const password = 'CSsuQkT2LqFaeX8';
+
+  const client = new matrixClient({ baseUrl: baseUrl, username: username, password: password });
+
+  client.on('onMessageRecv', (recvData) => {
+    console.log(recvData);
+    if (recvData.message.contains('hello')) {
+      const room_id = client.getRoomId(recvData.room);
+      if (room_id) {
+        client.sendMessage(room_id, 'Howdy!');
+      }
+    }
+  });
+
+  await client.connect();
+})();
